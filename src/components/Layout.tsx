5b40c76e2dcbe7dc75db5b6074c4d0fb1d5f3d98
Vytenis Kuciauskas <projektas@gmail.com>
2020-11-18 02:11:04 +0200

    Upgrade packages Add some dummy styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,7 +16,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <header>
-      <nav>
+      <nav className="fixed z-50 w-full bg-white top-0 flex flex-wrap items-center gap-2 px-2 py-3 shadow-lg">
         <Link href="/">
           <a>Home</a>
         </Link>{' '}
@@ -31,8 +31,8 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
         | <a href="/api/users">Users API</a>
       </nav>
     </header>
-    {children}
-    <footer>
+    <div className="mt-10 p-10 h-full">{children}</div>
+    <footer className="fixed z-50 w-full bg-gray-100 bottom-0 shadow-xl">
       <hr />
       <span>I'm here to stay (Footer)</span>
     </footer>
